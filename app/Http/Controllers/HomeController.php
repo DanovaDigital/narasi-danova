9ae756a0ef56e5a7ecc01bf39e00444650531ec4
Rangga Mukti <r4n994mukti@gmail.com>
2026-01-11 12:45:08 +0700

    feat(home): tweak HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use App\Models\Author;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
@@ -17,7 +18,7 @@ class HomeController extends Controller
                 ->published()
                 ->where('is_featured', true)
                 ->orderByDesc('published_at')
-                ->limit(4)
+                ->limit(3)
                 ->get();
         });
 
@@ -39,6 +40,17 @@ class HomeController extends Controller
                 ->get();
         });
 
+        // Must Read: High engagement articles (different from trending)
+        $mustRead = Cache::remember('home.must_read', 300, function () {
+            return Article::query()
+                ->published()
+                ->where('is_featured', false) // Exclude featured to avoid duplication
+                ->orderByDesc('views_count')
+                ->orderByDesc('published_at')
+                ->limit(4)
+                ->get();
+        });
+
         $topCategories = Cache::remember('home.top_categories', 300, function () {
             return Category::query()
                 ->withCount(['articles' => function ($q) {
@@ -46,7 +58,19 @@ class HomeController extends Controller
                 }])
                 ->orderByDesc('articles_count')
                 ->orderBy('name')
-                ->limit(8)
+                ->limit(10)
+                ->get();
+        });
+
+        // Top Creators: Authors with most articles
+        $topCreators = Cache::remember('home.top_creators', 300, function () {
+            return Author::query()
+                ->withCount(['articles' => function ($q) {
+                    $q->published();
+                }])
+                ->having('articles_count', '>', 0)
+                ->orderByDesc('articles_count')
+                ->limit(6)
                 ->get();
         });
 
@@ -54,7 +78,9 @@ class HomeController extends Controller
             'featured' => $featured,
             'trending' => $trending,
             'latest' => $latest,
+            'mustRead' => $mustRead,
             'topCategories' => $topCategories,
+            'topCreators' => $topCreators,
         ]);
     }
 }
